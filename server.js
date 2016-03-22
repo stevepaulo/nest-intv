4f384f85ee4bd6af848fb6ea6f4ccbd527062423
Steve Paulo <stevepaulo@gmail.com>
2016-03-22 16:38:31 -0700

    added randomized properties

--- server.js
+++ server.js
@@ -8,37 +8,101 @@ const router = express.Router();
 const port = process.env.PORT || 8012;
 const logger = require('./logger');
 const colors = ['blue', 'red', 'green', 'orange', 'yellow', 'purple', 'pink', 'brown', 'black', 'white', 'gray'];
-const products = ['thing', 'widget', 'sprocket', 'item', 'device', 'product', 'accessory', 'gadget', 'doohickey', 'whatsit'];
+const names = ['thing', 'widget', 'sprocket', 'item', 'device', 'product', 'accessory', 'gadget', 'doohickey', 'whatsit'];
+const origins = ['united states', 'mexico', 'canada', 'china', 'japan', 'korea', 'united kingdom', 'france', 'germany', 'morocco'];
+const props = [
+  {
+    name: 'name',
+    type: 'names',
+    p: 100
+  },
+  {
+    name: 'price',
+    type: 'number',
+    p: 95
+  },
+  {
+    name: 'quantity',
+    type: 'number',
+    p: 90
+  },
+  {
+    name: 'inStock',
+    type: 'boolean',
+    p: 85
+  },
+  {
+    name: 'availableColors',
+    type: 'colors',
+    p: 75
+  },
+  {
+    name: 'origin',
+    type: 'origins',
+    p: 50
+  }
+];
 
 const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getThisProductsProperties = randomizeProperties => {
+  let thisProductsProperties = JSON.parse(JSON.stringify(props));
+  if (randomizeProperties) {
+    thisProductsProperties.forEach((prop, i) => {
+      const diceRoll = getRandomIntInclusive(1, 100);
+      if (diceRoll > prop.p) {
+        thisProductsProperties.splice(i, 1);
+      }
+    });
+  }
+  return thisProductsProperties;
+};
 
-const generateResponse = maxDepth => {
-  maxDepth = maxDepth || 0;
+const generateResponse = (maxDepth, randomizeProperties) => {
   let returnArr = [];
-  let thisRequestsProductNames = JSON.parse(JSON.stringify(products));
+  let thisRequestsProductNames = JSON.parse(JSON.stringify(names));
+  let thisRequestsProductOrigins = JSON.parse(JSON.stringify(origins));
   let members = getRandomIntInclusive(1, 10);
   for (let i = 0; i < members; ++i) {
-    let thisMembersColorsArray = JSON.parse(JSON.stringify(colors));
-    let arrSize = getRandomIntInclusive(1, 5);
-    let relatedProducts = getRandomIntInclusive(0, maxDepth);
-    returnArr.push({
-      name: thisRequestsProductNames.splice(getRandomIntInclusive(0, (thisRequestsProductNames.length - 1)), 1)[0],
-      price: getRandomIntInclusive(1, 1000),
-      inStock: (i % 2 === 0) ? true : false,
-      availableColors: []
+    let thisProduct = {};
+    let thisProductsProperties = getThisProductsProperties(randomizeProperties);
+    thisProductsProperties.forEach(prop => {
+      switch(prop.type) {
+        case 'boolean':
+          if (getRandomIntInclusive(0,1) === 1) {
+            thisProduct[prop.name] = true;
+          } else {
+            thisProduct[prop.name] = false;
+          }
+          break;
+        case 'names':
+          thisProduct[prop.name] = thisRequestsProductNames.splice(getRandomIntInclusive(0, (thisRequestsProductNames.length - 1)), 1)[0];
+          break;
+        case 'origins':
+          thisProduct[prop.name] = thisRequestsProductOrigins.splice(getRandomIntInclusive(0, (thisRequestsProductOrigins.length - 1)), 1)[0];
+          break;
+        case 'colors':
+          let thisProductsColorsArray = JSON.parse(JSON.stringify(colors));
+          thisProduct[prop.name] = [];
+          for (let j = 0; j < getRandomIntInclusive(1,5); ++j) {
+            thisProduct[prop.name].push(thisProductsColorsArray.splice(getRandomIntInclusive(0, (thisProductsColorsArray.length - 1)), 1)[0]);
+          }
+          break;
+        default:
+          thisProduct[prop.name] = getRandomIntInclusive(1, 1000);
+      }
     });
-    for (let j = 0; j < arrSize; ++j) {
-      returnArr[i].availableColors.push(thisMembersColorsArray.splice(getRandomIntInclusive(0, (thisMembersColorsArray.length - 1)), 1)[0]);
-    }
+    returnArr.push(thisProduct);
+    let relatedProducts = getRandomIntInclusive(0, maxDepth);
     if (relatedProducts) {
-      returnArr[i].relatedProducts = generateResponse(maxDepth - 1);
+      returnArr[i].relatedProducts = generateResponse((maxDepth - 1), randomizeProperties);
     }
   }
   return returnArr;
 };
 
-router.get('/v1/products', (req, res) => res.status(200).json({products: generateResponse()}));
-router.get('/v2/products', (req, res) => res.status(200).json({products: generateResponse(2)}));
+router.get('/v1/products', (req, res) => res.status(200).json({products: generateResponse(0, false)}));
+router.get('/v2/products', (req, res) => res.status(200).json({products: generateResponse(0, true)}));
+router.get('/v3/products', (req, res) => res.status(200).json({products: generateResponse(2, true)}));
 router.get('/health', (req, res) => res.status(200).json({ message: 'OK' }));
 
 app.use((req, res, next) => {
